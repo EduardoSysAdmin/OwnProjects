64f973d3d5a02a5aad0d8ece18e3ca0e663f2e0a
EduardoSysAdmin <eduortiz.asir@gmail.com>
2019-11-30 00:57:20 +0100

    Create script.sh

---
+++ Bash/Named arguments/script.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+for i in "$@"
+do
+case $i in
+    --sftp-user=*|--SFTP-USER=*)
+    SFTPUSER="${i#*=}"
+    ;;
+    --sftp-server=*|--SFTP-SERVER=*)
+    SFTPSERVER="${i#*=}"
+    ;;
+    --DMP=*|--dmp=*)
+    DMP="${i#*=}"
+    ;;
+    --cdrs=*|--CDRS=*|--CDRs=*)
+    CDRS="${i#*=}"
+    ;;
+    *)
+    ;;
+esac
+
+done
+
+
+#Si cualquier var es null, sale...
+if [ -z "$SFTPUSER" ] || [ -z "$SFTPSERVER" ] || [ -z "$DMP" ] || [ -z "$CDRS" ];
+    then
+        echo "Al menos el valor de una de las variables es null.\nHay que declarar los parametros --sftp-user, --sftp-server, --dmp y --cdrs.\nSaliendo..."
+        
+    else
+        echo "$SFTPUSER"
+        echo "$CDRS"
+fi
